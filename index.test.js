8b12f45d05f28ab9ed989ccaa4ed77204dad4cba
Hendrik Luehrsen <hendrik@luehrsen.de>
2022-02-05 16:12:58 +0100

    Update test strategy

--- index.test.js
+++ index.test.js
@@ -1,13 +1,3 @@
-const postcss = require('postcss')
-
-const plugin = require('./')
-
-async function run (input, output, opts = { }) {
-  let result = await postcss([plugin(opts)]).process(input, { from: undefined })
-  expect(result.css).toEqual(output)
-  expect(result.warnings()).toHaveLength(0)
-}
-
-it('make sure the tests run', () => {
+it('makes sure the tests run', () => {
   return true;
 })
